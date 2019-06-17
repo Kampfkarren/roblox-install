841ef59026170277b8d49d40b95ceacbd8e40a75
Kampfkarren <boynedmaster@gmail.com>
2019-06-16 19:45:02 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use std::{io, path::PathBuf};
+
+#[cfg(target_os = "windows")]
+use winreg::RegKey;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    MalformedRegistry,
+    PlatformNotSupported,
+    RegistryError(io::Error),
+}
+
+#[must_use]
+pub struct RobloxStudio {
+    root: PathBuf,
+}
+
+impl RobloxStudio {
+    #[cfg(target_os = "windows")]
+    pub fn locate() -> Result<RobloxStudio> {
+        let hkcu = RegKey::predef(winreg::enums::HKEY_CURRENT_USER);
+
+        let roblox_studio_reg = hkcu
+            .open_subkey(r"Software\Roblox\RobloxStudio")
+            .map_err(Error::RegistryError)?;
+
+        let content_folder_value: String = roblox_studio_reg
+            .get_value("ContentFolder")
+            .map_err(Error::RegistryError)?;
+
+        let content_folder_path = PathBuf::from(content_folder_value);
+
+        Ok(RobloxStudio {
+            root: content_folder_path
+                .parent()
+                .ok_or(Error::MalformedRegistry)?
+                .to_owned(),
+        })
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    pub fn locate() -> Result<RobloxStudio> {
+        Err(Error::PlatformNotSupported)
+    }
+
+    #[must_use]
+    pub fn exe_path(&self) -> PathBuf {
+        self.root.join("RobloxStudioBeta.exe")
+    }
+
+    #[must_use]
+    pub fn built_in_plugins_path(&self) -> PathBuf {
+        self.root.join("BuiltInPlugins")
+    }
+}
---
+++ tests/tests.rs
@@ -0,0 +1,17 @@
+use roblox_install::RobloxStudio;
+
+#[cfg(target_os = "windows")]
+#[test]
+fn test_windows() {
+    let studio = RobloxStudio::locate().unwrap();
+
+    assert!(studio
+        .built_in_plugins_path()
+        .to_string_lossy()
+        .contains("BuiltInPlugins"));
+
+    assert!(studio
+        .exe_path()
+        .to_string_lossy()
+        .contains("RobloxStudioBeta.exe"));
+}
