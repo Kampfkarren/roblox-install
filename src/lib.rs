c845e808b2a03281378ef21f3288e3cce5fd7b37
Olivier Trepanier <jeparlefrancais21@gmail.com>
2019-08-02 16:58:46 -0700

    Fix windows root field

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,7 @@ impl RobloxStudio {
             .ok_or(Error::MalformedRegistry)?.join("Plugins");
 
         Ok(RobloxStudio {
-            root: root.to_owned(),
+            root: root.to_path_buf(),
             exe: root.join("RobloxStudioBeta.exe"),
             built_in_plugins: root.join("BuiltInPlugins"),
             plugins: plugins.to_owned(),
